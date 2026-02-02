0c2ee67ca2147f3b46a3845de540ff7e2e62ea24
Yout701 <harshkumarworld@gmail.com>
2026-02-02 07:59:08 +0530

    a

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,5 +1,10 @@
 import CheckoutPage from "@/src/components/checkout/checkout";
+import { Suspense } from "react";
 
 export default async function Chechkout() {
-  return <CheckoutPage />;
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <CheckoutPage />
+    </Suspense>
+  );
 }
